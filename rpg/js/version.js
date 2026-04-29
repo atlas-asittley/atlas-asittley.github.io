3ace635b0acac878045e417929369e70312f1c2c
Atlas <atlas.asittley@gmail.com>
2026-04-29 19:34:08 -0400

    Update Mucklo RPG link to standalone repo

--- rpg/js/version.js
+++ rpg/js/version.js
@@ -1,6 +1,6 @@
 // AUTO-GENERATED — do not edit
 const GAME_VERSION = {
-  commit: '5aa84dd',
+  commit: '96437d1',
   date: '2026-04-29 23:33 UTC',
-  message: 'Update Mucklo Boy's Day link to standalone repo'
+  message: 'Remove duplicated Mucklo Boy's Day from hub repo'
 };
